f7d688b4a03f6a522947faf8aa80d191bbf69ca5
flamingo99900 <kryglykov200512345@gmail.com>
2025-01-22 15:50:53 +0300

    DOne!

---
+++ src/main/java/Message.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int threadNum;
+    private String message;
+    private long time;
+}
