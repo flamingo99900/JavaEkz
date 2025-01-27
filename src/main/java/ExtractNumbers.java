0f93bd71352af1113ca19d5f10a97f5d1a9e1c0f
Ognimalf <71066418+flamingo99900@users.noreply.github.com>
2025-01-27 14:32:00 +0300

    done

---
+++ src/main/java/ExtractNumbers.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ExtractNumbers {
+    public static void main(String[] args) {
+        String input = "I have 3 cats, 4 dogs, and 1 turtle";
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (char ch : input.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                numbers.add(Character.getNumericValue(ch));
+            }
+        }
+
+        int[] resultArray = numbers.stream().mapToInt(Integer::intValue).toArray();
+
+        System.out.print("Извлеченные числа: ");
+        for (int num : resultArray) {
+            System.out.print(num + " ");
+        }
+    }
+}
