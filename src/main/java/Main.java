a77951018ae7b701884fa9d40fe4359da40150a7
Ognimalf <71066418+flamingo99900@users.noreply.github.com>
2025-01-27 15:25:34 +0300

    done

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+// 8. Разработать программу для вывода на экран кубов первых десяти положительных
+//чисел.
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Задание 8");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false); // только для чтения
+
+        StringBuilder taskText = new StringBuilder(" Кубы первых десяти положительных чисел: \n");
+
+        for (int i = 1; i < 11; i++){
+            int result = i * i * i;
+            taskText.append(i).append("^3 = ").append(result).append("\n");
+        }
+
+        textArea.setText(taskText.toString());
+        frame.add(textArea);
+        frame.setVisible(true);
+    }
+
+}
